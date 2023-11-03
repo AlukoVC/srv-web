b26c9ede1373a77e9c77f505adf7b5b28081404e
Aluko <97165284+AlukoVC@users.noreply.github.com>
2023-11-03 15:34:06 +0100

    Update variables.tf

--- module/instance/variables.tf
+++ module/instance/variables.tf
@@ -25,6 +25,18 @@ variable "security_group_ids" {
   description = "Liste des ID des groupes de sécurité pour l'instance EC2"
 }
 
+variable "vpc_cidr_block" {
+  description = "CIDR block for the VPC"
+}
+
+variable "subnet_cidr_block" {
+  description = "CIDR block for the subnet"
+}
+
+variable "availability_zone" {
+  description = "Availability zone for the subnet"
+}
+
 variable "security_group_name" {
   description = "Nom du groupe de sécurité"
 }
