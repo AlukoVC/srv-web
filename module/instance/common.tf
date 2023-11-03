498fe755b822fcff098064eb38ae484ca62cecee
Aluko <97165284+AlukoVC@users.noreply.github.com>
2023-11-03 16:04:03 +0100

    Update common.tf

--- module/instance/common.tf
+++ module/instance/common.tf
@@ -1,1 +1,17 @@
+resource "aws_vpc" "example" {
+  cidr_block = var.vpc_cidr_block
+  enable_dns_support = true
+  enable_dns_hostnames = true
 
+  tags = {
+    Name = "example-vpc"
+  }
+}
+
+resource "aws_internet_gateway" "example" {
+  vpc_id = aws_vpc.example.id
+
+  tags = {
+    Name = "example-internet-gateway"
+  }
+}
