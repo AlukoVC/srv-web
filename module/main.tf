24f9d03bc7f4fc0f4329503787f0feb18a7b4dac
Aluko <97165284+AlukoVC@users.noreply.github.com>
2023-11-03 15:02:11 +0100

    Update main.tf

--- module/main.tf
+++ module/main.tf
@@ -8,3 +8,32 @@ module "network" {
   subnet_cidr_block   = "10.0.1.0/24"
   availability_zone    = "us-east-1a"
 }
+
+module "web_server" {
+  source              = "./modules/web_server"
+  ami_id              = "ami-01bc990364452ab3e"  # Change this to your desired AMI
+  instance_type       = "t2.micro"
+  key_name            = "key-pair"  # Change this to your key pair name
+  subnet_id           = module.network.subnet_id
+  security_group_ids  = [module.network.security_group_id]  # Use the security group from the network module
+  user_data_script    = <<-EOF_SCRIPT
+                          #!/bin/bash -xe
+                          yum update -y
+                          yum install -y httpd git
+                          service httpd start
+                          chkconfig httpd on
+                          git clone https://bitbucket.org/fhoubart/testphaser_aws.git /var/www/html/testphaser_aws
+                          cat <<EOF >> /etc/httpd/conf/httpd.conf
+                          <VirtualHost *:80>
+                              DocumentRoot "/var/www/html/testphaser_aws/public_html/"
+                              <Directory "/var/www/html/testphaser_aws/public_html/">
+                                  Options Indexes FollowSymLinks
+                                  AllowOverride All
+                                  Require all granted
+                              </Directory>
+                          </VirtualHost>
+                          EOF
+                          service httpd restart
+                          EOF_SCRIPT
+  instance_name       = "web-server-instance"
+}
