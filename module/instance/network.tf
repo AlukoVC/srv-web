995381426bc3c2700cceacc0e8781da02e02679f
Aluko <97165284+AlukoVC@users.noreply.github.com>
2023-11-03 16:05:43 +0100

    Update network.tf

--- module/instance/network.tf
+++ module/instance/network.tf
@@ -1,1 +1,10 @@
+resource "aws_subnet" "example" {
+  vpc_id                  = aws_vpc.example.id
+  cidr_block              = var.subnet_cidr_block
+  availability_zone       = var.availability_zone
+  map_public_ip_on_launch = true
 
+  tags = {
+    Name = "example-subnet"
+  }
+}
