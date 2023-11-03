af05bf2bdbf86d08876b1d48d6c5e9421904847b
Aluko <97165284+AlukoVC@users.noreply.github.com>
2023-11-03 14:56:17 +0100

    Update main.tf

--- module/main.tf
+++ module/main.tf
@@ -1,1 +1,10 @@
+provider "aws" {
+  region = "us-east-1"
+}
 
+module "network" {
+  source              = "./modules/network"
+  vpc_cidr_block      = "10.0.0.0/16"
+  subnet_cidr_block   = "10.0.1.0/24"
+  availability_zone    = "us-east-1a"
+}
