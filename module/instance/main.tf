433865d588567fd0d6cc3e2fdae3b7804a70f125
Aluko <97165284+AlukoVC@users.noreply.github.com>
2023-11-03 16:05:36 +0100

    Update main.tf

--- module/instance/main.tf
+++ module/instance/main.tf
@@ -1,14 +1,3 @@
-resource "aws_subnet" "example" {
-  vpc_id                  = aws_vpc.example.id
-  cidr_block              = var.subnet_cidr_block
-  availability_zone       = var.availability_zone
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "example-subnet"
-  }
-}
-
 resource "aws_instance" "example" {
   ami           = var.ami_id
   instance_type = var.instance_type
