27e48111423980bc508e5ee3df4b67c38913801f
Aluko <97165284+AlukoVC@users.noreply.github.com>
2023-11-03 14:38:00 +0100

    Update main.tf

--- module/securitygroup/main.tf
+++ module/securitygroup/main.tf
@@ -1,1 +1,26 @@
+resource "aws_security_group" "example" {
+  name        = "example-serveur-web"
+  description = "Regles de securite pour le serveur web"
+  vpc_id      = aws_vpc.example.id
 
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["${aws_instance.example3.private_ip}/32"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
