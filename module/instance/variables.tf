3b38f5036eda3c7368ae311fc054e0e440fb0dee
Aluko <97165284+AlukoVC@users.noreply.github.com>
2023-11-03 14:38:49 +0100

    Update variables.tf

--- module/instance/variables.tf
+++ module/instance/variables.tf
@@ -5,7 +5,6 @@ variable "ami_id" {
 
 variable "instance_name" {
   description = "Nom de l'instance EC2"
-  default     = "srv-web"
 }
 
 variable "instance_type" {
@@ -20,33 +19,12 @@ variable "key_name" {
 
 variable "subnet_id" {
   description = "ID du sous-réseau pour l'instance EC2"
-  # Assurez-vous de fournir une valeur appropriée au moment de l'utilisation
 }
 
 variable "security_group_ids" {
   description = "Liste des ID des groupes de sécurité pour l'instance EC2"
-  # Assurez-vous de fournir une valeur appropriée au moment de l'utilisation
 }
 
 variable "user_data_script" {
   description = "Script de démarrage de l'instance EC2"
-  default = <<-EOF_SCRIPT
-              #!/bin/bash -xe
-              yum update -y
-              yum install -y httpd git
-              service httpd start
-              chkconfig httpd on
-              git clone https://bitbucket.org/fhoubart/testphaser_aws.git /var/www/html/testphaser_aws
-              cat <<EOF >> /etc/httpd/conf/httpd.conf
-              <VirtualHost *:80>
-                  DocumentRoot "/var/www/html/testphaser_aws/public_html/"
-                  <Directory "/var/www/html/testphaser_aws/public_html/">
-                      Options Indexes FollowSymLinks
-                      AllowOverride All
-                      Require all granted
-                  </Directory>
-              </VirtualHost>
-              EOF
-              service httpd restart
-              EOF_SCRIPT
 }
