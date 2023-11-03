b5b5558149bf6809567f5a229be4ffbeba8321e5
Aluko <97165284+AlukoVC@users.noreply.github.com>
2023-11-03 15:33:42 +0100

    Update variables.tf

--- module/instance/variables.tf
+++ module/instance/variables.tf
@@ -25,6 +25,22 @@ variable "security_group_ids" {
   description = "Liste des ID des groupes de sécurité pour l'instance EC2"
 }
 
+variable "security_group_name" {
+  description = "Nom du groupe de sécurité"
+}
+
+variable "security_group_description" {
+  description = "Description du groupe de sécurité"
+}
+
+variable "vpc_id" {
+  description = "ID du VPC pour le groupe de sécurité"
+}
+
+variable "ssh_ingress_ip" {
+  description = "Adresse IP autorisée pour SSH ingress"
+}
+
 variable "user_data_script" {
   description = "Script de démarrage de l'instance EC2"
 }
