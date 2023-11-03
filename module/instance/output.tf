dabcc6e96f6a68cc4971eda94feb102e4fb407ae
Aluko <97165284+AlukoVC@users.noreply.github.com>
2023-11-03 15:31:56 +0100

    Update output.tf

--- module/instance/output.tf
+++ module/instance/output.tf
@@ -5,3 +5,12 @@ output "web_server_instance_id" {
 output "web_server_private_ip" {
   value = aws_instance.example.private_ip
 }
+
+output "vpc_id" {
+  value = aws_vpc.example.id
+}
+
+output "subnet_id" {
+  value = aws_subnet.example.id
+}
+
