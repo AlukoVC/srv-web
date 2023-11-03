e8b6b59d0d3f657f79f9003b26797a6bfe1de076
Aluko <97165284+AlukoVC@users.noreply.github.com>
2023-11-03 14:45:54 +0100

    Update main.tf

--- module/network/main.tf
+++ module/network/main.tf
@@ -1,1 +1,47 @@
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
 
+  tags = {
+    Name = "example-vpc"
+  }
+}
+
+resource "aws_internet_gateway" "example" {
+  vpc_id = aws_vpc.example.id
+
+  tags = {
+    Name = "example-internet-gateway"
+  }
+}
+
+resource "aws_route_table_association" "example" {
+  subnet_id      = aws_subnet.example.id
+  route_table_id = aws_route_table.example.id
+}
+
+resource "aws_subnet" "example" {
+  vpc_id                  = aws_vpc.example.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a" 
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "example-subnet"
+  }
+}
+
+resource "aws_route_table" "example" {
+  vpc_id = aws_vpc.example.id
+
+  tags = {
+    Name = "example-route-table"
+  }
+}
+
+resource "aws_route" "example" {
+  route_table_id            = aws_route_table.example.id
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id                = aws_internet_gateway.example.id
+}
